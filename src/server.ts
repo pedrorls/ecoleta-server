135026710e801bf1289f9e62d0cf3055cb26c73b
Pedro Silva <pedro.rls93@gmail.com>
2020-06-26 20:13:05 -0300

    feat(tables): create project tables

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Listagem de usuários");
+app.use(express.json());
 
-  response.json(["pedro", "pedro", "pedro", "pedro"]);
-});
+app.use(routes);
 
 app.listen(3333);
